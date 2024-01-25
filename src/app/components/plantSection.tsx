62a62fba67d5f014369cbbd3044448805da32273
koume <koume.go@salto.link>
2024-01-25 22:06:26 +0900

    おんぼろパソ故画面の確認ができないので一旦あげちゃう、、ごめんよ

--- src/app/components/plantSection.tsx
+++ src/app/components/plantSection.tsx
@@ -3,90 +3,63 @@ import Image from "next/image";
 
 const plantSection = () => {
   return (
-    <div className=".container mx-auto flex flex-wrap">
-      {/* Cactus */}
-      <div className="w-full md:w-1/4">
-        <div className="mb-8">
-          <Image
-            src={"/img/Cactus.jpg"}
-            alt="Cactus"
-            height={280}
-            width={187}
-            layout="responsive"
-          ></Image>
-          <div className="text-center">
-            <a href="#"
-              className="text-black"
-            >
-              Cactus
-            </a>
-          </div>
+    <div>
+      <div className="flex py-28 md:px-10 relative">
+        <div className="text-center">
+          <a href="#" className="hover:scale-110 hover:cursor-pointer">
+           <Image
+             src={"/img/Cactus.jpg"}
+             alt="Cactus"
+             height={300}
+             width={298}
+             layout="responsive"
+            ></Image>
+          </a>
+          <a href="#" className="font-serif pt-3 text-black hover:border-green-600">
+              Cactus →
+          </a>
         </div>
-      </div>
-
-      {/* Bonsai */}
-      <div className="w-full md:w-1/4">
-        <div className="mb-8">
+        <div className="">
+          <a href="#" className="-mb-5 hover:scale-110">
           <Image
-            className="w-full h-auto mb-4 object-cover"
             src={"/img/Bonsai.jpg"}
             alt="Bonsai"
             height={280}
             width={187}
             layout="responsive"
           ></Image>
-          <div className="text-center">
-            <a
-              href="#"
-              className="inline-block bg-blue-500 text-white py-2 px-4 rounded-full"
-            >
-              Bonsai
-            </a>
-          </div>
+          </a>
+          <a href="#" className="font-serif pt-3 text-black hover:border-green-600">
+              Bonsai →
+          </a>
         </div>
-      </div>
-
-      {/* Succulent */}
-      <div className="w-full md:w-1/4">
-        <div className="mb-8">
+        <div className="">
+          <a href="#" className="-mb-5 hover:scale-110">
           <Image
-            className="w-full h-auto mb-4 object-cover"
             src={"/img/Succulent.jpg"}
             alt="Succulent"
             height={280}
             width={187}
             layout="responsive"
           ></Image>
-          <div className="text-center">
-            <a
-              href="#"
-              className="inline-block bg-yellow-500 text-white py-2 px-4 rounded-full"
-            >
-              Succulent
-            </a>
-          </div>
+          </a>
+          <a href="#" className="font-serif pt-3 text-black hover:border-green-600">
+              Succulent →
+          </a>
         </div>
-      </div>
-
-      {/*  Indoor Plants*/}
-      <div className="w-full md:w-1/4">
-        <div className="mb-8">
+        <div className="">
+          <a href="#" className="-mb-5 hover:scale-110">
           <Image
-            className="w-full h-auto mb-4 object-cover"
             src={"/img/IndoorPlants.jpg"}
             alt="IndoorPlants"
             height={280}
             width={187}
             layout="responsive"
           ></Image>
-          <div className="text-center">
-            <a
-              href="#"
-              className="inline-block bg-purple-500 text-white py-2 px-4 rounded-full"
-            >
-              Indoor Plants
-            </a>
-          </div>
+          </a>
+          <a href="#" className="font-serif pt-3 text-black hover:border-green-600">
+              Indoor Plants →
+          </a>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Sns from "./components/sns";
-import PlantSection from "./components/plantSection copy";
+import PlantSection from "./components/plantSection";
+import PlantText from "./components/plantText";
 
 function page() {
   return (
     <div>
       <Header/>
+      <PlantText/>
       <PlantSection/>
       <Sns />
       <Footer />
