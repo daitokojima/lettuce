62ff830a5988d4a7fd95080bb317c4f8d7b18b20
koume <koume.go@salto.link>
2024-01-30 00:06:26 +0900

    一旦形にはなりましたがレスポンシブが、、です

--- src/app/components/plantSection.tsx
+++ src/app/components/plantSection.tsx
@@ -4,61 +4,69 @@ import Image from "next/image";
 const plantSection = () => {
   return (
     <div>
-      <div className="flex py-28 md:px-10 relative">
-        <div className="text-center">
-          <a href="#" className="hover:scale-110 hover:cursor-pointer">
-           <Image
-             src={"/img/Cactus.jpg"}
-             alt="Cactus"
-             height={300}
-             width={298}
-             layout="responsive"
-            ></Image>
-          </a>
-          <a href="#" className="font-serif pt-3 text-black hover:border-green-600">
-              Cactus →
-          </a>
-        </div>
-        <div className="">
-          <a href="#" className="-mb-5 hover:scale-110">
-          <Image
-            src={"/img/Bonsai.jpg"}
-            alt="Bonsai"
-            height={280}
-            width={187}
-            layout="responsive"
-          ></Image>
-          </a>
-          <a href="#" className="font-serif pt-3 text-black hover:border-green-600">
-              Bonsai →
+      <div className="flex w-full items-center mx-auto pt-12 relative">
+        <div className="md:w-[30%] text-center w-full ">
+          <div className='hover:scale-110 ease-in duration-500'>
+            <a href="#" className="-mb-5">
+              <Image
+                src={"/img/Cactus.png"}
+                alt="Cactus"
+                height={80}
+                width={100}
+                layout="responsive"
+              />
+            </a>
+          </div>
+          <a href="#" className="font-serif text-3xl hover:underline decoration-green-600">
+            Cactus →
           </a>
         </div>
-        <div className="">
-          <a href="#" className="-mb-5 hover:scale-110">
-          <Image
-            src={"/img/Succulent.jpg"}
-            alt="Succulent"
-            height={280}
-            width={187}
-            layout="responsive"
-          ></Image>
-          </a>
-          <a href="#" className="font-serif pt-3 text-black hover:border-green-600">
-              Succulent →
+        <div className="md:w-[30%] text-center w-full ">
+          <div className='hover:scale-110 ease-in duration-500'>
+            <a href="#" className="-mb-5">
+              <Image
+                src={"/img/Bonsai.png"}
+                alt="Bonsai"
+                height={80}
+                width={100}
+                layout="responsive"
+              />
+            </a>
+          </div>
+          <a href="#" className="font-serif text-3xl hover:underline decoration-green-600">
+            Bonsai →
           </a>
         </div>
-        <div className="">
-          <a href="#" className="-mb-5 hover:scale-110">
-          <Image
-            src={"/img/IndoorPlants.jpg"}
-            alt="IndoorPlants"
-            height={280}
-            width={187}
-            layout="responsive"
-          ></Image>
+        <div className="md:w-[30%] text-center w-full ">
+          <div className='hover:scale-110 ease-in duration-500'>
+            <a href="#" className="-mb-5">
+              <Image
+                src={"/img/Succulent.png"}
+                alt="Succulent"
+                height={80}
+                width={100}
+                layout="responsive"
+              />
+            </a>
+          </div>
+          <a href="#" className="font-serif text-3xl hover:underline decoration-green-600">
+            Succulent →
           </a>
-          <a href="#" className="font-serif pt-3 text-black hover:border-green-600">
-              Indoor Plants →
+        </div>
+        <div className="md:w-[30%] text-center w-full ">
+          <div className='hover:scale-110 ease-in duration-500'>
+            <a href="#" className="-mb-5">
+              <Image
+                src={"/img/IndoorPlants.png"}
+                alt="Indoor Plants"
+                height={80}
+                width={100}
+                layout="responsive"
+              />
+            </a>
+          </div>
+          <a href="#" className="font-serif text-3xl hover:underline decoration-green-600">
+            Indoor Plants →
           </a>
         </div>
       </div>
--- src/app/components/plantText.tsx
+++ src/app/components/plantText.tsx
@@ -2,45 +2,76 @@ import React from 'react'
 import Image from "next/image";
 
 function plantText() {
-    return (
-        <div>
-            <div className='md:flex'>
-                <div className='flex py-10 px-10'>
-                  <Image
-                      src={"/img/forest.svg"}
-                      alt="icon1"
-                      height={32}
-                      width={32}
-                      layout="responsive"
-                  ></Image>
-                  <h2 className='font-serif pt-3'>High Quality Plants</h2>
-                  <p className='font-extralight'>A line about the service<br/> you&apos;ve mentioned above.</p>
-                </div>
-                <div className='flex'>
-                  <Image
-                     src={"/img/eda.svg"}
-                     alt="icon2"
-                     height={32}
-                     width={32}
-                     layout="responsive"
-                  ></Image>
-                  <h2 className='font-serif pt-3'>Wide Plant Range</h2>
-                  <p className='font-extralight'>A line about the service<br/> you&apos;ve mentioned above.</p>
-                </div>
-                <div className='flex'>
-                  <Image
-                     src={"/img/king.svg"}
-                     alt="icon2"
-                     height={32}
-                     width={32}
-                     layout="responsive"
-                  ></Image>
-                  <h2 className='font-serif pt-3'>Fast Delivery</h2>
-                  <p className='font-extralight'>A line about the service<br/> you&apos;ve mentioned above.</p>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="flex mx-auto w-full items-center pt-12 relative">
+        <div className='mx-auto flex justify-center items-center gap-4'>
+          <div className='md:w-[70px] mr-4'>
+            <Image
+              src={"/img/forest.svg"}
+              alt="icon1"
+              height={5}
+              width={5}
+              layout="responsive"
+            />
+          </div>
+          <div className='flex-col leading-4'>
+            <h2 className='font-serif text-2xl font-bold'>High Quality<br /> Plants</h2>
+            <br />
+            <p className='font-extralight text-xl'>A line about the service<br /> you&apos;ve mentioned above.</p>
+          </div>
         </div>
-    )
+        <div className='mx-auto flex justify-center items-center gap-4'>
+          <div className='md:w-[70px] mr-4'>
+            <Image
+              src={"/img/eda.svg"}
+              alt="icon1"
+              height={5}
+              width={5}
+              layout="responsive"
+            />
+          </div>
+          <div className='flex-col leading-4'>
+            <h2 className='font-serif text-2xl font-bold'>Wide Plant Range</h2>
+            <br />
+            <p className='font-extralight text-xl'>A line about the service<br /> you&apos;ve mentioned above.</p>
+          </div>
+        </div>
+        <div className='mx-auto flex justify-center items-center gap-4'>
+          <div className='md:w-[70px] mr-4'>
+            <Image
+              src={"/img/king.svg"}
+              alt="icon1"
+              height={5}
+              width={5}
+              layout="responsive"
+            />
+          </div>
+          <div className='flex-col leading-4'>
+            <h2 className='font-serif text-2xl font-bold'>Excellent Services</h2>
+            <br />
+            <p className='font-extralight text-xl'>A line about the service<br /> you&apos;ve mentioned above.</p>
+          </div>
+        </div>
+        <div className='mx-auto flex justify-center items-center gap-4'>
+          <div className='md:w-[70px] mr-4'>
+            <Image
+              src={"/img/van.svg"}
+              alt="icon1"
+              height={5}
+              width={5}
+              layout="responsive"
+            />
+          </div>
+          <div className='flex-col leading-4'>
+            <h2 className='font-serif text-2xl font-bold'>Fast Delivery</h2>
+            <br />
+            <p className='font-extralight text-xl'>A line about the service<br /> you&apos;ve mentioned above.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default plantText
